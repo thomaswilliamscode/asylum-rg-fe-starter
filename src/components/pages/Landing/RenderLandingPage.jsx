b8b7de2d1b5960cc47b43c27f0d732bd8f036510
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2024-07-02 01:49:09 -0400

    Ticket 1 Complete

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -32,6 +32,32 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
+      <div className="graphs-section">
+        <div className="gr-office-img">
+          <img
+            src={GrantRatesByOfficeImg}
+            alt="Grant Rates By Office Graph"
+            class="gr-office-img"
+          />
+          <p>Search Grant Rates By Office</p>
+        </div>
+        <div className="gr-nationality-img">
+          <img
+            src={GrantRatesByNationalityImg}
+            alt="Search Grant Rates By Nationality"
+            class="gr-office-img"
+          />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+        <div className="gr-overtime-img">
+          <img
+            src={GrantRatesOverTimeImg}
+            alt="Search Grant Rates Over Time"
+            class="gr-office-img"
+          />
+          <p>Search Grant Rates Over Time</p>
+        </div>
+      </div>
       {/* <div className="graphs-section"> */}
       <div className="view-more-data-btn-container">
         <Button
@@ -41,6 +67,7 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        <Button>Download the Data</Button>
       </div>
 
       <div className="middle-section">
@@ -61,6 +88,37 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className="data-container">
+            <div className="data1">
+              <h4>36%</h4>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+            <div className="data2">
+              <h4>5%</h4>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+            <div className="data3">
+              <h4>6x Lower</h4>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office’s
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+          <div className='read-more'>
+            <Button>Read More</Button>
+          </div>
+        </div>
         {/* <div className="bottom-section">*/}
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
